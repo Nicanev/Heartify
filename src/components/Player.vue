0877cce7fb5c63181f9cc0c1f9686a4e6c971cd4
Nikita <nic7ven@gmail.com>
2022-07-16 21:50:56 +0400

    add volume

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -5,7 +5,7 @@
         <img src="../static/img/album-image.png" alt="album-image" />
       </div>
       <div class="track-info__description">
-        <div class="track-info__title"><a href="#"> Deceiver</a></div>
+        <div class="track-info__title"><a href="#"> Stricken</a></div>
         <div class="track-info__performer"><a href="#">Disturbed</a></div>
       </div>
     </div>
@@ -100,7 +100,102 @@
         <div class="player__time-end time">{{ formatTimeEnd }}</div>
       </div>
     </div>
-    <div class="player__volume">Volume</div>
+    <div class="player__volume volume">
+      <div class="volume__icon" @click="mute">
+        <svg
+          v-if="statusVolume > 0 && statusVolume < 70"
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M15.7117 4.73972C15.6835 4.5659 15.4671 4.47296 15.3137 4.62495L12.0842 7.826C11.0073 8.89337 9.54604 9.49312 8.02227 9.49312C7.76985 9.49312 7.56522 9.69572 7.56522 9.94564V14.0544C7.56522 14.3043 7.76985 14.5069 8.02227 14.5069C9.54604 14.5069 11.0073 15.1066 12.0842 16.174L15.3137 19.3751C15.4671 19.527 15.6835 19.4341 15.7117 19.2603C16.0244 17.3313 16.4348 14.3455 16.4348 12C16.4348 9.65454 16.0244 6.66871 15.7117 4.73972ZM14.2064 3.52973C15.235 2.51017 17.0202 3.03295 17.2571 4.4941C17.5715 6.43286 18 9.52248 18 12C18 14.4775 17.5715 17.5671 17.2571 19.5059C17.0202 20.967 15.235 21.4898 14.2064 20.4703L10.9768 17.2692C10.1935 16.4928 9.13062 16.0566 8.02227 16.0566C6.9054 16.0566 6 15.1602 6 14.0544V9.94564C6 8.83984 6.9054 7.94341 8.02227 7.94341C9.13062 7.94341 10.1935 7.50717 10.9768 6.73079L14.2064 3.52973Z"
+            fill="#BABABA"
+          />
+          <path
+            d="M19 9C20.2806 9.79573 21 10.8748 21 12C21 13.1252 20.2806 14.2043 19 15"
+            stroke="#BABABA"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <svg
+          v-if="statusVolume >= 70"
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M13.7117 4.73972C13.6835 4.5659 13.4671 4.47296 13.3137 4.62495L10.0842 7.826C9.0073 8.89337 7.54604 9.49312 6.02227 9.49312C5.76985 9.49312 5.56522 9.69572 5.56522 9.94564V14.0544C5.56522 14.3043 5.76985 14.5069 6.02227 14.5069C7.54604 14.5069 9.0073 15.1066 10.0842 16.174L13.3137 19.3751C13.4671 19.527 13.6835 19.4341 13.7117 19.2603C14.0244 17.3313 14.4348 14.3455 14.4348 12C14.4348 9.65454 14.0244 6.66871 13.7117 4.73972ZM12.2064 3.52973C13.235 2.51017 15.0202 3.03295 15.2571 4.4941C15.5715 6.43286 16 9.52248 16 12C16 14.4775 15.5715 17.5671 15.2571 19.5059C15.0202 20.967 13.235 21.4898 12.2064 20.4703L8.9768 17.2692C8.1935 16.4928 7.13062 16.0566 6.02227 16.0566C4.9054 16.0566 4 15.1602 4 14.0544V9.94564C4 8.83984 4.9054 7.94341 6.02227 7.94341C7.13062 7.94341 8.1935 7.50717 8.9768 6.73079L12.2064 3.52973Z"
+            fill="#BABABA"
+          />
+          <path
+            d="M17 9C18.2806 9.79573 19 10.8748 19 12C19 13.1252 18.2806 14.2043 17 15"
+            stroke="#BABABA"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M18 7C19.9209 8.32622 21 10.1247 21 12C21 13.8753 19.9209 15.6738 18 17"
+            stroke="#BABABA"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <svg
+          v-if="statusVolume == 0"
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M13.7117 4.73972C13.6835 4.5659 13.4671 4.47296 13.3137 4.62495L10.0842 7.826C9.0073 8.89337 7.54604 9.49312 6.02227 9.49312C5.76985 9.49312 5.56522 9.69572 5.56522 9.94564V14.0544C5.56522 14.3043 5.76985 14.5069 6.02227 14.5069C7.54604 14.5069 9.0073 15.1066 10.0842 16.174L13.3137 19.3751C13.4671 19.527 13.6835 19.4341 13.7117 19.2603C14.0244 17.3313 14.4348 14.3455 14.4348 12C14.4348 10.3176 14.2236 8.30568 13.9888 6.59068C13.8963 5.9148 13.8001 5.28503 13.7117 4.73972ZM12.2064 3.52973C13.235 2.51017 15.0202 3.03295 15.2571 4.4941C15.5715 6.43286 16 9.52248 16 12C16 14.4775 15.5715 17.5671 15.2571 19.5059C15.0202 20.967 13.235 21.4898 12.2064 20.4703L8.9768 17.2692C8.1935 16.4928 7.13062 16.0566 6.02227 16.0566C4.9054 16.0566 4 15.1602 4 14.0544V9.94564C4 8.83984 4.9054 7.94341 6.02227 7.94341C7.13062 7.94341 8.1935 7.50717 8.9768 6.73079L12.2064 3.52973Z"
+            fill="#BABABA"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M17.2375 14.6458C17.1063 14.5226 17.1014 14.318 17.2265 14.1889L22.0247 9.2339C22.1498 9.10472 22.3575 9.09986 22.4887 9.22305C22.6199 9.34624 22.6248 9.55082 22.4997 9.68L17.7015 14.635C17.5764 14.7641 17.3687 14.769 17.2375 14.6458Z"
+            fill="#B3B3B3"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M22.6355 14.6458C22.7667 14.5226 22.7716 14.318 22.6465 14.1889L17.8483 9.2339C17.7232 9.10472 17.5155 9.09986 17.3843 9.22305C17.2531 9.34624 17.2482 9.55082 17.3733 9.68L22.1715 14.635C22.2966 14.7641 22.5044 14.769 22.6355 14.6458Z"
+            fill="#B3B3B3"
+          />
+        </svg>
+      </div>
+      <div class="volume__progressbar">
+        <div class="volume__slider-container">
+          <input
+            type="range"
+            name="volume-track"
+            id="volume-track"
+            v-model="statusVolume"
+            min="0"
+            max="100"
+            step="10"
+          />
+          <div
+            class="progress-volume"
+            :style="{ width: statusVolume + '%' }"
+          ></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -110,6 +205,7 @@ export default {
     return {
       songs: [{ title: "test", duration: 247 }],
       statusSong: 0,
+      statusVolume: 20,
       isPlay: false,
       formatTimeEnd: "",
       formatTimeStart: "0:00",
@@ -141,13 +237,17 @@ export default {
         } else {
           this.statusSong = nowTime;
         }
-      }, 250);
+      }, 1000);
+    },
+    statusVolume() {
+      this.audio.volume = this.statusVolume / 100;
     },
   },
   methods: {
     play() {
       if (!this.isPlay) {
         this.audio.play();
+        this.audio.volume = this.statusVolume / 100;
         this.isPlay = true;
       } else {
         this.pause();
@@ -156,6 +256,9 @@ export default {
     pause() {
       this.audio.pause();
       this.isPlay = false;
+    },
+    mute() {
+      this.statusVolume = 0;
     },
     rewind() {
       this.rewindStatus = true;
@@ -288,6 +391,63 @@ export default {
       text-decoration: underline;
       color: white;
     }
+  }
+}
+.volume {
+  display: flex;
+  align-items: center;
+  padding-right: 20px;
+  gap: 5px;
+  &__icon {
+    margin-top: 10px;
+    cursor: pointer;
+  }
+  &__slider-container {
+    width: 100px;
+    position: relative;
+  }
+  &__progressbar {
+    input {
+      appearance: none;
+      height: 8px;
+      width: 100%;
+      background: #35383e;
+      border-radius: 6px;
+    }
+    input::-webkit-slider-thumb {
+      appearance: none;
+      position: relative;
+      z-index: 100;
+      width: 15px;
+      height: 15px;
+      border-radius: 50%;
+      background: transparent;
+      cursor: pointer;
+      transition: 0.2s;
+    }
+    .progress-volume {
+      position: absolute;
+      top: 9px;
+      appearance: none;
+      height: 8px;
+      left: 0px;
+      width: 20%;
+      z-index: 99;
+      pointer-events: none;
+      background: linear-gradient(90deg, #2de66f 0%, #18d25b 100%);
+      border-radius: 6px;
+    }
+  }
+  &__progressbar:hover {
+  }
+}
+.volume:hover {
+  input::-webkit-slider-thumb {
+    background: linear-gradient(90deg, #2ae46d 0%, #0fca52 100%);
+    transition: 0.2s;
+  }
+  svg path {
+    fill: white;
   }
 }
 .circle {
